870b06191242354604b76c8bf28cc923f76457f2
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-01 15:34:21 +0530

    Bidding System

--- bidder.py
+++ bidder.py
@@ -4,9 +4,9 @@ while ans == "Yes" or ans == "yes" or ans == "YES":
     n = input("Name : ")
     amt = int(input("Bid Amount : "))
     bids[n] = amt
-    ans = input("Anymore bidders ?")
+    ans = input("Anymore bidders ? ")
 
 a = max(bids.values())
 for keys in bids:
     if bids[keys] == a:
-        print("Higest bidder is ", keys)
+        print("Higest bidder is  : ", keys)
